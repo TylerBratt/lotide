2b17d77bfe3d0574d1b67a2d98e274abc0a3a31e
Tyler Bratt <tyler~bratt@hotmail.com>
2021-04-05 15:13:15 -0400

    refactored for mocha

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`🍀🍀🍀 Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🤬🤬🤬 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  //if the arrays are of different length - this cannot be true
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  //Loop through the arrays' length
-  for (let i = 0; i < array1.length; i++) {
-    //If an indexed value is not the same in the other array - this cannot be true
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  //if all other tests fail - the arrays must be equal
-  return true;
-};
 
 const middle = function(array1) {
   let middleNumber = Math.floor(array1.length / 2);
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,14 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
+describe('#middle', () => {
+  it('should return 2 for [1, 2, 3]', () => {
+    assertArraysEqual(middle([1, 2, 3]), 2);
+  });
+  it('should return 3, 4 for [1, 2, 3, 4, 5, 6]', () => {
+    assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), 3, 4);
+  });
+});
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
