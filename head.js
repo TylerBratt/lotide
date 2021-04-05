f0a1eb0f53ba11a1f30bc69ab453c89d2287df80
Tyler Bratt <tyler~bratt@hotmail.com>
2021-04-05 14:41:43 -0400

    added Mocha and Chai dependancied to head.js and headTest.js

--- head.js
+++ head.js
@@ -1,7 +1,7 @@
 const assertEqual = require('./assertEqual');
 
 const head = function(actual) {
-  for (let i = 0; i < actual.length - 1; i++) {
+  for (let i = 0; i < actual.length; i++) {
     return actual[0];
   }
 };
